bd52a6f2232c8f6667cc25b6d719b63aa8e9f53c
ONG KEAN YEW <TP045130@mail.apu.edu.my>
2019-07-08 14:38:21 +0800

    Add project files.

---
+++ TestAppForGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAppForGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
